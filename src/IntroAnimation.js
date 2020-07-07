dfff983541d3ac7728a946ff94a38eef87ce34ee
Connor <cstreng95@gmail.com>
2020-07-07 21:14:38 +0200

    continues work on sky

--- src/IntroAnimation.js
+++ src/IntroAnimation.js
@@ -1,57 +1,24 @@
 import React from 'react'
 import logo from './logo.png'
+import WaveRow from './WaveRow'
 
 const IntroAnimation = () => {
+
+    const waveRows = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
     return (
         <div className="intro-animation-container">
             <div className="logo-container">
-            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/WIKIPEDIA15_BATTLESHIP.svg/1024px-WIKIPEDIA15_BATTLESHIP.svg.png" alt='logo' />
-                Battleship
+                {/* <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/WIKIPEDIA15_BATTLESHIP.svg/1024px-WIKIPEDIA15_BATTLESHIP.svg.png" alt='logo' /> */}
             </div>
-            <div className="intro-sky">
+
+            <div className="sky">
                 <div className="sun"></div>
             </div>
-            <div className="wave-container">
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
+            <div className="wave-parent">
+                { waveRows.map((row, rowIndex) => {
+                return <WaveRow number = { row } />
+            }) }
             </div>
 
         </div>
---
+++ src/Waves.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Waves = (props) => {
+    
+    return (
+        <div className={`wave position-${props.position}`}>
+        </div>
+    )
+}
+
+export default Waves;
