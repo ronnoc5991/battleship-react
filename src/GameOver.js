0c323875b692c4fc1434da371de41764f545cefa
Connor <cstreng95@gmail.com>
2020-08-29 21:54:10 +0200

    end of day

--- src/GameOver.js
+++ src/GameOver.js
@@ -4,6 +4,16 @@ function GameOver() {
 
     return (
         <div className="game-over">
+            <div className="rolling-wave wave-1"></div>
+            <div className="rolling-wave wave-2"></div>
+            <div className="rolling-wave wave-3"></div>
+            <div className="rolling-wave wave-4"></div>
+            <div className="rolling-wave wave-5"></div>
+            <div className="rolling-wave wave-6"></div>
+            <div className="rolling-wave wave-7"></div>
+            <div className="rolling-wave wave-8"></div>
+            <div className="rolling-wave wave-9"></div>
+            <div className="rolling-wave wave-10"></div>
         </div>
     )
 }
