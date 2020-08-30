6f026bacd1b76198bcbe1bcb1a08e32ca6a3413b
Connor <cstreng95@gmail.com>
2020-08-30 20:43:59 +0200

    Updates

---
+++ precache-manifest.ed29536140ff1a67ded0154be3c220dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7162828a6338390920f1c13c7fae4e2d",
+    "url": "/battleship-react/index.html"
+  },
+  {
+    "revision": "c29bd2a1840cf087e4f0",
+    "url": "/battleship-react/static/css/main.3361e004.chunk.css"
+  },
+  {
+    "revision": "e9d0f73c2db503253ba6",
+    "url": "/battleship-react/static/js/2.37bbf42c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship-react/static/js/2.37bbf42c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29bd2a1840cf087e4f0",
+    "url": "/battleship-react/static/js/main.6c3edd24.chunk.js"
+  },
+  {
+    "revision": "571a2ae2df6bc3d8c81c",
+    "url": "/battleship-react/static/js/runtime-main.7585f23a.js"
+  }
+]);
